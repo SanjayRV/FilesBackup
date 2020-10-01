f28971b3194d0c4c6648dedc76892b1d3bef3803
BETSOL\Sanjay.Rampura <sanjay.rampura@betsol.com>
2020-10-01 11:30:40 +0000

    Files Backup

--- goworkspace/src/APITraining/Auth/models.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// var users = map[string]string{
-// 	"user1": "password1",
-// 	"user2": "password2",
-// }
--- goworkspace/src/APITraining/SampleDbAPI/models.go
+++
@@ -1,7 +0,0 @@
-package main
-
-//Post - storing
-type Post struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-}
